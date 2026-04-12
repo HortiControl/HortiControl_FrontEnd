d0b06c6420bb52aebbb1773c1d2c0f8610beff34
Gustavo01833 <gustavo.opereira@sptech.school>
2026-04-12 19:28:49 -0300

    Tela de cadastro e login integradas

--- frontend/src/components/Login/CadastroCard.jsx
+++ frontend/src/components/Login/CadastroCard.jsx
@@ -2,8 +2,59 @@ import { User, Mail, Phone, Lock } from 'lucide-react';
 import { Button } from "../Button";
 import banner from "../../assets/banner.png";
 import logo from "../../assets/HortiControlLogo.png";
+import { useState } from "react";
 
-const CadastroCard = () => {
+
+const CadastroCard = () => {  
+
+    //Para realizar o envio das informações do
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Captura as informações do formulário
+        const formData = new FormData(e.currentTarget);
+
+        // Transforma em objeto json
+        const dados = Object.fromEntries(formData.entries());
+
+        console.log(dados);
+        
+        if (dados.senha !== dados.confirmarSenha) {
+            alert("As senhas não coincidem!");
+            return;
+        }
+
+        try {
+            const response = await fetch("http://localhost:8080/usuarios", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nome: dados.nome,
+                    email: dados.email,
+                    telefone: dados.telefone,
+                    senha: dados.senha
+                })
+            });
+
+            console.log(response);
+
+            if (!response.ok) throw new Error();
+
+            alert("Cadastro realizado com sucesso!");
+        }
+        catch (error) {
+            console.log(error.message);
+            alert("Erro ao cadastrar");
+        }
+
+
+    };
+
+
+
+
     return (
         <div className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat font-[Montserrat] p-4"
             style={{ backgroundImage: `url(${banner})` }}>
@@ -15,13 +66,14 @@ const CadastroCard = () => {
                 <h2 className="text-[#333] font-bold text-3xl mb-1">Criar uma conta</h2>
                 <p className="text-gray-500 text-sm mb-6 text-center">Preencha os dados abaixo para se cadastrar</p>
 
-                <form className="w-full space-y-4">
+                <form className="w-full space-y-4" onSubmit={handleSubmit}>
                     <div className="flex flex-col">
                         <label className="text-gray-700 font-semibold mb-1 ml-1 text-sm">Nome Completo:</label>
                         <div className="relative">
                             <User className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="text"
+                                name='nome'
                                 placeholder="Seu nome"
                                 className="w-full pl-10 pr-4 py-2 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
@@ -34,6 +86,7 @@ const CadastroCard = () => {
                             <Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="email"
+                                name='email'
                                 placeholder="exemplo@email.com"
                                 className="w-full pl-10 pr-4 py-2 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
@@ -45,7 +98,9 @@ const CadastroCard = () => {
                         <div className="relative">
                             <Phone className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
+                                maxLength={11}
                                 type="text"
+                                name='telefone'
                                 placeholder="(11) 91234 5678"
                                 className="w-full pl-10 pr-4 py-2 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
@@ -58,6 +113,7 @@ const CadastroCard = () => {
                             <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="password"
+                                name='senha'
                                 placeholder="Mínimo 5 caracteres"
                                 className="w-full pl-10 pr-4 py-2 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
@@ -70,6 +126,7 @@ const CadastroCard = () => {
                             <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="password"
+                                name='confirmarSenha'
                                 placeholder="Digite a senha novamente"
                                 className="w-full pl-10 pr-4 py-2 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
--- frontend/src/components/Login/LoginCard.jsx
+++ frontend/src/components/Login/LoginCard.jsx
@@ -4,7 +4,57 @@ import banner from "../../assets/banner.png";
 import logo from "../../assets/HortiControlLogo.png";
 
 const LoginCard = () => {
+
+
+    //Para realizar o envio das informações do
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Captura as informações do formulário
+        const formData = new FormData(e.currentTarget);
+
+        // Transforma em objeto json
+        const dados = Object.fromEntries(formData.entries());
+
+        console.log(dados);
+        
+        // if (dados.senha !== dados.confirmarSenha) {
+        //     alert("As senhas não coincidem!");
+        //     return;
+        // }
+
+        try {
+            const response = await fetch("http://localhost:8080/usuarios/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: dados.email,
+                    senha: dados.senha
+                }),
+            });
+
+            console.log(response);
+
+            if (!response.ok) throw new Error();
+            const token = await response.text();
+            
+            // Se precisar do token, dê um localStorage.getItem("token");
+            localStorage.setItem("token",token);
+            // console.log(token);
+            alert("Login realizado com sucesso!");
+        }
+        catch (error) {
+            console.log(error.message);
+            alert("Erro ao cadastrar");
+        }
+
+
+    };
+    
     return (
+
         <div className="min-h-screen w-full flex items-center justify-center bg-cover bg-center bg-no-repeat font-[Montserrat] p-4"
             style={{ backgroundImage: `url(${banner})` }}>
 
@@ -15,13 +65,14 @@ const LoginCard = () => {
                 <h2 className="text-[#333] font-bold text-3xl mb-1">Seja Bem-Vindo!</h2>
                 <p className="text-gray-500 text-sm mb-8">Faça o login e acesse o sistema</p>
 
-                <form className="w-full space-y-5">
+                <form className="w-full space-y-5" onSubmit={handleSubmit}>
                     <div className="flex flex-col">
                         <label className="text-gray-700 font-semibold mb-1.5 ml-1 text-sm">E-mail:</label>
                         <div className="relative">
                             <Mail className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="email"
+                                name='email'
                                 placeholder="exemplo@email.com"
                                 className="w-full pl-10 pr-4 py-3 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
@@ -34,6 +85,7 @@ const LoginCard = () => {
                             <Lock className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
                             <input
                                 type="password"
+                                name='senha'
                                 placeholder="•••••"
                                 className="w-full pl-10 pr-10 py-3 bg-[#e9ecef] rounded-xl border-none focus:ring-2 focus:ring-[#009951] outline-none transition-all placeholder:text-gray-400"
                             />
