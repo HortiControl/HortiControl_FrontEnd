9ef105e1c44a715bac1c3ab06bb47fdffaa5c3fc
Mateus <mateus.jimenez@sptech.school>
2026-06-22 10:47:11 -0300

    Teste de backend integrado

--- frontend/src/provider/api.js
+++ frontend/src/provider/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://horticontrol-backend.onrender.com"
+    // baseURL: "http://localhost:8080"
 })
 
 export default api;
